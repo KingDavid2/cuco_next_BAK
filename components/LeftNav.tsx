861abfb41a753fac0fa6a4276fa12b146f38646c
David Nahum <david.caldera@yale.edu>
2021-08-18 13:17:14 -0600

    LeftNav add ids

--- components/LeftNav.tsx
+++ components/LeftNav.tsx
@@ -6,11 +6,11 @@ import Paper from '@material-ui/core/Paper';
 
 const LeftNav = () => {
   const [menuItems, setMenuItems] = useState([
-    { text: 'Home', icon: '', link: '/' },
-    { text: 'Artists', icon: '', link: '/artists' },
-    { text: 'Artworks', icon: '', link: '/artworks' },
-    { text: 'Collective', icon: '', link: '/collective' },
-    { text: 'Events', icon: '', link: '/events' },
+    { id: 1, text: 'Home', icon: '', link: '/' },
+    { id: 2, text: 'Artists', icon: '', link: '/artists' },
+    { id: 3, text: 'Artworks', icon: '', link: '/artworks' },
+    { id: 4, text: 'Collective', icon: '', link: '/collective' },
+    { id: 5, text: 'Events', icon: '', link: '/events' },
   ])
 
   return (
@@ -18,7 +18,7 @@ const LeftNav = () => {
       <MenuList>
         {menuItems.map(item => { 
           return (
-            <Link href={item.link} passHref>
+            <Link key={item.id} href={item.link} passHref>
               <MenuItem>{item.text}</MenuItem>
             </Link>
           )
