689da4d5340c7cf36c7453b8c68c384f82a53193
David Nahum <david.caldera@yale.edu>
2021-08-18 08:32:58 -0600

    Add interface prop type to DefaultLayout

--- layouts/DefaultLayout.tsx
+++ layouts/DefaultLayout.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { FC } from "react";
 import LeftNav from  '../components/LeftNav'
 import { Grid } from '@material-ui/core';
 
-const DefaultLayout = ({ children }) => {
+interface Props {
+  children: object
+}
+
+const DefaultLayout: FC<Props> = ({ children }) => {
   return (
     <Grid
       container
-      spacing={0}
+      spacing={1}
       direction="row"
       justifyContent="flex-start"
       alignItems="flex-start"
     >
-      <LeftNav/>
+      <Grid item xs={2}>
+        <LeftNav/>
+      </Grid>
       { children}
     </Grid>
-
   );
 }
 
