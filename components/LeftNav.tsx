edc19d5569bc2e645760bb5e9300cde742be88ac
David Nahum <david.caldera@yale.edu>
2021-08-18 08:34:56 -0600

    LeftNav iterate menuItems

--- components/LeftNav.tsx
+++ components/LeftNav.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import MenuList from '@material-ui/core/MenuList';
 import MenuItem from '@material-ui/core/MenuItem';
 import Paper from '@material-ui/core/Paper';
 
 const LeftNav = () => {
+  const [menuItems, setMenuItems] = useState([
+    { text: 'Home', icon: '', link: '/' },
+    { text: 'Artists', icon: '', link: '/artists' },
+    { text: 'Artworks', icon: '', link: '/artworks' },
+    { text: 'Collective', icon: '', link: '/collective' },
+    { text: 'Events', icon: '', link: '/events' },
+  ])
+
   return (
     <Paper>
       <MenuList>
-        <MenuItem>Profile</MenuItem>
-        <MenuItem>My account</MenuItem>
-        <MenuItem>Logout</MenuItem>
+        {menuItems.map(item => { 
+          return (
+            <Link href={item.link} passHref>
+              <MenuItem>{item.text}</MenuItem>
+            </Link>
+          )
+        })}
       </MenuList>
     </Paper>
   );
