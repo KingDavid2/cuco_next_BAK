bd0feef9e995525022a212cf21b10adb74595767
David Nahum <david.caldera@yale.edu>
2021-08-23 13:22:05 -0600

    User faker to mock data

--- api_server/mockData.js
+++ api_server/mockData.js
@@ -1,69 +1,26 @@
 const { v4: uuidv4 } = require('uuid');
+const faker = require('faker');
+const sample = require('lodash.sample');
 
-const artists = [
-  {
+const randNum = (min, max) => {
+  return Math.round(Math.random() * (max - min)) + min;
+}
+
+const randBool = () => {
+  return randNum(0,1) ? true : false
+}
+
+const artists = [...Array(8)].map( (_,i) => {
+  return {
     id: uuidv4(),
-    name: 'Pablo',
-    lastName: 'Picasso',
-    link: '/',
-    subheader: 'Some description',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tempor lectus ac leo maximus, eu pulvinar magna luctus. Vestibulum eget nisi et nulla tristique pellentesque non vitae diam. Donec sem enim, semper sed efficitur sit amet, commodo in quam. Vestibulum molestie leo gravida lectus convallis imperdiet.',
-    image: 'https://pablo-picasso.space/wp-content/uploads/2019/07/pablo-picasso-facts-thumbnail-2.jpg'
-  },
-  {
-    id: uuidv4(),
-    name: 'Pablo',
-    lastName: 'Picasso',
-    link: '/',
-    subheader: 'Some description',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tempor lectus ac leo maximus, eu pulvinar magna luctus. Vestibulum eget nisi et nulla tristique pellentesque non vitae diam. Donec sem enim, semper sed efficitur sit amet, commodo in quam. Vestibulum molestie leo gravida lectus convallis imperdiet.',
-    image: 'https://pablo-picasso.space/wp-content/uploads/2019/07/pablo-picasso-facts-thumbnail-2.jpg'
-  },
-  {
-    id: uuidv4(),
-    name: 'Pablo',
-    lastName: 'Picasso',
-    link: '/',
-    subheader: 'Some description',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tempor lectus ac leo maximus, eu pulvinar magna luctus. Vestibulum eget nisi et nulla tristique pellentesque non vitae diam. Donec sem enim, semper sed efficitur sit amet, commodo in quam. Vestibulum molestie leo gravida lectus convallis imperdiet.',
-    image: 'https://pablo-picasso.space/wp-content/uploads/2019/07/pablo-picasso-facts-thumbnail-2.jpg'
-  },
-  {
-    id: uuidv4(),
-    name: 'Pablo', lastName: 'Picasso',
-    link: '/',
-    subheader: 'Some description',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tempor lectus ac leo maximus, eu pulvinar magna luctus. Vestibulum eget nisi et nulla tristique pellentesque non vitae diam. Donec sem enim, semper sed efficitur sit amet, commodo in quam. Vestibulum molestie leo gravida lectus convallis imperdiet.',
-    image: 'https://pablo-picasso.space/wp-content/uploads/2019/07/pablo-picasso-facts-thumbnail-2.jpg'
-  },
-  {
-    id: uuidv4(),
-    name: 'Pablo',
-    lastName: 'Picasso',
-    link: '/',
-    subheader: 'Some description',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tempor lectus ac leo maximus, eu pulvinar magna luctus. Vestibulum eget nisi et nulla tristique pellentesque non vitae diam. Donec sem enim, semper sed efficitur sit amet, commodo in quam. Vestibulum molestie leo gravida lectus convallis imperdiet.',
-    image: 'https://pablo-picasso.space/wp-content/uploads/2019/07/pablo-picasso-facts-thumbnail-2.jpg'
-  },
-  {
-    id: uuidv4(),
-    name: 'Pablo',
-    lastName: 'Picasso',
-    link: '/',
-    subheader: 'Some description',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tempor lectus ac leo maximus, eu pulvinar magna luctus. Vestibulum eget nisi et nulla tristique pellentesque non vitae diam. Donec sem enim, semper sed efficitur sit amet, commodo in quam. Vestibulum molestie leo gravida lectus convallis imperdiet.',
-    image: 'https://pablo-picasso.space/wp-content/uploads/2019/07/pablo-picasso-facts-thumbnail-2.jpg'
-  },
-  {
-    id: uuidv4(),
-    name: 'Pablo',
-    lastName: 'Picasso',
-    link: '/',
-    subheader: 'Some description',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tempor lectus ac leo maximus, eu pulvinar magna luctus. Vestibulum eget nisi et nulla tristique pellentesque non vitae diam. Donec sem enim, semper sed efficitur sit amet, commodo in quam. Vestibulum molestie leo gravida lectus convallis imperdiet.',
-    image: 'https://pablo-picasso.space/wp-content/uploads/2019/07/pablo-picasso-facts-thumbnail-2.jpg'
-  }
-]
+    name: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    link: faker.internet.url,
+    subheader: faker.lorem.words(2),
+    description: faker.lorem.words(5),
+    image: faker.internet.avatar()
+  }}
+)
 
 // Using CommonJS style export so we can consume via Node (without using Babel-node)
 module.exports = {
