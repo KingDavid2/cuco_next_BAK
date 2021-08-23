591101c4ac65ce442d9d77c74e4313951cf6ec5a
David Nahum <david.caldera@yale.edu>
2021-08-23 13:27:00 -0600

    Add Artworks mock data

--- api_server/createMockDb.js
+++ api_server/createMockDb.js
@@ -2,8 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const mockData = require("./mockData");
 
-const { artists } = mockData;
-const data = JSON.stringify({ artists });
+const data = JSON.stringify(mockData);
 const filepath = path.join(__dirname, "db.json");
 
 fs.writeFile(filepath, data, function(err) {
--- api_server/mockData.js
+++ api_server/mockData.js
@@ -21,8 +21,33 @@ const artists = [...Array(8)].map( (_,i) => {
     image: faker.internet.avatar()
   }}
 )
+const user_ids = artists.map( artist => artist.id )
+
+const artworks = [...Array(8)].map( (_,i) => {
+  const user_id = sample(user_ids)
+  const quantity = randNum(0,2)
+  const can_sell = quantity > 0 ? randBool() : false
+
+  return {
+    id: uuidv4(),
+    user_id: user_id,
+    title: faker.lorem.words(3),
+    description: faker.lorem.words(5),
+    height: randNum(1, 20),
+    depth: randNum(0, 2),
+    width: randNum(1, 20),
+    can_sell: can_sell,
+    price: randNum(50, 100),
+    quantity: quantity,
+    images: [
+      faker.image.image(),
+      faker.image.image()
+    ]
+  }}
+)
 
 // Using CommonJS style export so we can consume via Node (without using Babel-node)
 module.exports = {
-  artists
+  artists,
+  artworks
 };
